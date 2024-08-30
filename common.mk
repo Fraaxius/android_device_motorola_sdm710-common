3066a0e22b836c806325447c68fcc2bd258bcc20
Fraaxius <franthekillaz@gmail.com>
2024-08-30 00:30:18 -0400

    sdm710-common: astro: Switch to prebuilt kernel & headers

--- common.mk
+++ common.mk
@@ -302,6 +302,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Kernel
+TARGET_KERNEL_DIR ?= device/motorola/astro-kernel
+LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image
+
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
+# Kernel Headers
+PRODUCT_VENDOR_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
